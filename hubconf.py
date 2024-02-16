4a7e79e9d0cb4934fde7ef73588fe548cf4349f0
Ace <agtogonon@up.edu.ph>
2024-02-16 11:50:03 +0800

    fixed typo

--- hubconf.py
+++ hubconf.py
@@ -88,7 +88,7 @@ def custom(path="./fasterrcnn_model_drinks_Epoch9.pt", autoshape=True, _verbose=
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn()
     num_classes = 4             # 3 drinks + background
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictorFastRCNNPredictor(in_features, num_classes)
+    model.roi_heads.box_predictor = torchvision.models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
     
     model_path = path      # edit epoch as needed
 
